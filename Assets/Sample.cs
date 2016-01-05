63db352ef4c9af5b3537291521eb09a0598a9f11
kiyoaki <kiyoaki.tsurutani@gmail.com>
2016-01-05 19:10:15 +0900

    Initial commit

---
+++ Assets/Sample.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+using UniRx;
+using UnityEngine.UI;
+
+public class Sample : MonoBehaviour
+{
+    public string Url;
+
+    public Button YieldReturnNullButton;
+    public Button YieldReturnWwwButton;
+
+    void Start()
+    {
+        YieldReturnNullButton.OnClickAsObservable().Subscribe(_ =>
+        {
+            ObservableWWW.Get(Url, null, new Progress<float>(f => { }))
+                .Timeout(TimeSpan.FromSeconds(1))
+                .Subscribe();
+        });
+
+        YieldReturnWwwButton.OnClickAsObservable().Subscribe(_ =>
+        {
+            ObservableWWW.Get(Url)
+                .Timeout(TimeSpan.FromSeconds(1))
+                .Subscribe();
+        });
+    }
+}
